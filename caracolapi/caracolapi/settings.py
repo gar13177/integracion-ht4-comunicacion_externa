c03c1657fe8e8a26aebdc48f3eab973849828cdb
Kevin García <gar13177@uvg.edu.gt>
2017-08-29 23:45:14 -0600

    refactorización de código. cambio de base de datos

--- caracolapi/caracolapi/settings.py
+++ caracolapi/caracolapi/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'caracolapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'caracol',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
